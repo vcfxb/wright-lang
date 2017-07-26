269aa62512ee0d0473be4bf2da928fbe2341196d
Antonia Calia-Bogan <alfriadox@gmail.com>
2017-07-25 23:52:09 -0400

    Finished ArgParser

--- wright/src/main.rs
+++ wright/src/main.rs
@@ -2,11 +2,19 @@ extern crate wright;
 
 use std::{env, process};
 use wright::argparse;
+use wright::run;
 
 fn main() {
     // todo
     if let Some(i) = argparse::argparse(env::args()) {
-        println!("{:?}", i);
+        //println!("{:?}", i);
+        if i.input == argparse::InputMode::Interactive {
+            process::exit(run::interactive());
+        } else if i.run {
+            process::exit(run::interpret(i.mode, i.file.unwrap()));
+        } else if let Some(out) = i.output {
+            process::exit(run::compile(i.mode, i.file.unwrap(), i.optimization, out));
+        }
     } else {
         process::exit(0);
     }
