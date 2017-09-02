ef47f8015385c8ba87df5a59400286d7a3d16aa9
Antonia Calia-Bogan <alfriadox@gmail.com>
2017-09-02 03:41:20 -0400

    Update travis, tests

--- wright/tests/preproc_tests.rs
+++ wright/tests/preproc_tests.rs
@@ -3,5 +3,10 @@ extern crate wright;
 use wright::preproc::*;
 #[test]
 fn single_line_comments() {
-    assert_eq!(preproc("hey // hey".to_string()), "hey ".to_string());
+    assert_eq!(preproc("hey //comment".to_string()), "hey ".to_string());
+}
+
+#[test]
+fn multi_line_comments() {
+    assert_eq!(preproc("h/* comment */ey".to_string()), "hey".to_string());
 }
