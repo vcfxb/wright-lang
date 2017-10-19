6c35b3a07f40328453bce67aaaa08bea91f3a03b
Antonia Calia-Bogan <alfriadox@gmail.com>
2017-10-18 22:48:42 -0400

    Update Travis, Remove LLVM
    Removing lvm (For now at least) due to complications in the continuous integration builds

--- wright/src/lib.rs
+++ wright/src/lib.rs
@@ -2,7 +2,8 @@ pub mod lexer;
 pub mod preproc;
 pub mod parser;
 pub mod argparse;
-pub mod radon;
+// removed in favor of llvm at one point. might re-add.
+//pub mod radon;
 pub mod run;
 pub mod version;
 pub mod errors;
