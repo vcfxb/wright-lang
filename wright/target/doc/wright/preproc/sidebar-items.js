3a3d38169f84a68f2a97b664315ad88ede376e77
Antonia Calia-Bogan <alfriadox@gmail.com>
2017-10-22 01:16:14 -0400

    Revert

--- wright/target/doc/wright/preproc/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["preproc",""]],"struct":[["NumberedLine","`NumberedLine`s are the returned type of the preprocessor. They have all comments removed based on the profile used. Numbering for the `line_number` field stars at 1."]]});
