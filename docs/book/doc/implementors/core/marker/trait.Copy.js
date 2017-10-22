7fae207170fc600e59a708f6ea26c030e24cdafa
Antonia Calia-Bogan <alfriadox@gmail.com>
2017-10-22 02:15:40 -0400

    Fix glitch/bug

---
+++ docs/book/doc/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["wright"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"wright/errors/enum.WrightErrorLevels.html\" title=\"enum wright::errors::WrightErrorLevels\">WrightErrorLevels</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
