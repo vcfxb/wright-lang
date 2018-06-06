6e3b71b99c76a1517b6975110d3a895897518134
Antonia Calia-Bogan <alfriadox@gmail.com>
2018-06-06 17:29:45 -0400

    Work done

--- wright/src/interpreter/interactive.rs
+++ wright/src/interpreter/interactive.rs
@@ -1,4 +1,6 @@
+//! Module for interactive evaluation of wright statements and files.
 
+/// Starts a REPL through the standard input / output. Currently unimplemented.
 pub fn interactive() -> i32 {
     unimplemented!()
 }
