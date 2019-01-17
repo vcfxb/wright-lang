9fb887be51c147821fc162a6ad2a015a32c8ee4f
Antonia Calia-Bogan <alfriadox@gmail.com>
2019-01-17 16:14:27 -0500

    Committing before branching
    Will give the entire project a bit of a refresh and restart in the new branch

--- wright/tests/parser_tests.rs
+++ wright/tests/parser_tests.rs
@@ -4,7 +4,10 @@ use wright::grammar::*;
 #[test]
 fn test_hello_world() {
     let prog = r#"
-fn main() {println("Hello from Wright");}
+use prelude
+func main() {
+    println("Hello from Wright");
+}
     "#;
-    println!("{:#?}", program(prog));
+    println!("{:#?}", parse_program(prog));
 }
