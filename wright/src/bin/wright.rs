5dc03fbac525aac9db04020b3a1aa0a99d6f69fe
Antonia Calia-Bogan <alfriadox@gmail.com>
2019-02-27 14:59:40 -0500

    Rewrite try 1
    I'm switching back to the codespan crate i think

--- wright/src/bin/wright.rs
+++ wright/src/bin/wright.rs
@@ -1,59 +1,20 @@
-// todo
 
-//use std::process::exit;
-use clap::{Arg, App, AppSettings};
-use crate::version::VERSION;
-use regex::Regex;
+use wright::{Emit, call_files};
+use wright::cli::get_wright_app;
+use std::process::exit;
 
 fn main() {
-    let matches = App::new("Wright")
-        .setting(AppSettings::ColorAlways)
-        .version(VERSION)
-        .author("Antonia Calia-Bogan (github.com/Alfriadox)")
-        .about("The Wright programming language interpreter and compiler.")
-        .arg(Arg::with_name("INPUT")
-            .required_unless("INTERACTIVE")
-            .help("Input wright file.")
-            .validator(is_valid_wright_file_name)
-        )
-        .arg(Arg::with_name("RUN")
-            .requires("INPUT")
-            .help("Runs input wright file rather than compiling it.")
-            .case_insensitive(true)
-            .short("r")
-            .long("run")
-        )
-        .arg(Arg::with_name("EMIT")
-            .short("e")
-            .long("emit")
-            .help("Prints intermediate representation(s).")
-            .takes_value(true)
-            .possible_values(&["tokens", "ast"])
-            .use_delimiter(true)
-            .multiple(true)
-            .conflicts_with("INTERACTIVE")
-            .requires("INPUT")
-        )
-        .get_matches();
-//    let file = matches.value_of("INPUT").unwrap();
-//    let mut emits: Vec<Emit> = Vec::with_capacity(3);
-//    if matches.is_present("EMIT") {
-//        for v in matches.values_of("EMIT").unwrap() {
-//            emits.push(match v {
-//                "tokens"  => Emit::Tokens,
-//                "ast"     => Emit::AbstractSyntaxTree,
-//                other => panic!("{} should not be a possible emit option.", other),
-//            });
-//        }
-//    }
-//    exit(match Interpreter::treewalker(file, emits) {Some(i) => i.run(), _ => 1});
-}
-
-fn is_valid_wright_file_name(s: String) -> Result<(), String> {
-    let re: Regex = Regex::new(r"[[:word:]]+.wr$|[[:word:]]+.wright$").unwrap(); 
-    match re.is_match(s.as_str()) {
-        true  => Ok(()),
-        false => Err(format!("Wright file names can only contain alphanumerics and underscores and \
-        must end with .wr or .wright. ({} does not.)", s)),
+    let matches = get_wright_app().get_matches();
+    let filenames = matches.values_of("INPUT").unwrap().collect();
+    let mut emits: Vec<Emit> = Vec::with_capacity(3);
+    if matches.is_present("EMIT") {
+        for v in matches.values_of("EMIT").unwrap() {
+            emits.push(match v {
+                "tokens"  => Emit::Tokens,
+                "ast"     => Emit::AbstractSyntaxTree,
+                other => panic!("{} should not be a possible emit option.", other),
+            });
+        }
     }
+    exit(call_files(filenames, matches.is_present("RUN"), emits, matches.is_present("VERBOSE")))
 }
