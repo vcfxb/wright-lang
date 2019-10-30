96a8a3c9fe6e89cf528868581f2a81d81c943f77
Antonia Calia-Bogan <alfriadox@gmail.com>
2019-10-30 15:10:21 -0400

    PEST parser 1

--- wright/tests/parser.rs
+++ wright/tests/parser.rs
@@ -1,50 +1,24 @@
-use wright::grammar::parser::ignored as ign_parser;
-use wright::grammar::parser::integer_literal;
-use wright::grammar::parser::identifier;
+#[macro_use]
+extern crate pest;
+
+use wright::grammar::parser::{WrightParser, Rule};
+
 
 #[test]
 fn ignored() {
-    let tests = [
-        " ",
-        "",
-        "# comment \n",
-        "#* m comment \n *#",
-        "#* a #* nested *# comment *#",
-        "#* nested #* a *##*b*#c \n*#",
-        "#* double #* nested #* comment *# a *#a *#c",
-    ];
-    for s in tests.iter() {
-        eprintln!("{}", s);
-        ign_parser().parse(s.as_bytes()).unwrap();
-    }
+
 }
 
 #[test]
 fn integers() {
-    let tests = [
-        "0",
-        "1000",
-        "0xFFfa1982001",
-        "0b010100010",
-        "4",
-    ];
-    for s in tests.iter() {
-        eprintln!("{}", s);
-        integer_literal().parse(s.as_bytes()).unwrap();
-    }
+    parses_to!(
+        parser: WrightParser,
+        input: "1000",
+        rule:  Rule::NUM_LITERAL,
+        tokens: [ NUM_LITERAL(0,4, [ DEC_LITERAL(0,4) ]) ]
+    )
 }
 
 #[test]
 fn ident() {
-    fn ok(s: &'static str) {
-        assert!(identifier().parse(s.as_bytes()).is_ok())
-    }
-    fn err(s: &'static str) {
-        assert!(identifier().parse(s.as_bytes()).is_err())
-    }
-    ok("_identifier");
-    err("20");
-    ok("test");
-    err("true");
-    err("_");
 }
