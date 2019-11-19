25b6606b22d5dda8972f4c6c9dee51cf0b901602
Antonia Calia-Bogan <alfriadox@gmail.com>
2019-11-18 21:16:15 -0500

    Working on lexer

--- wright/src/grammar.rs
+++ wright/src/grammar.rs
@@ -7,4 +7,13 @@ use codespan::{
 
 pub mod ast;
 pub mod parser;
+
+/// Module for Wright's lexer. Parts of wright's grammar are defined in here, namely:
+///
+/// - `# ...` is a wright single line comment.
+/// - `#* ... *#` is a wright multiline comment.
+/// - `#? ...` is a wright doc comment.
+/// - `## ... ##` is a wright doc comment over multiple lines.
+/// - `#! ...` is a wright doc comment for the module. (like `//!` in rust.)
+///
 pub mod lexer;
