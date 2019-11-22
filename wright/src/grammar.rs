686851ed12cf13154a9183f04d755bed1d47353a
Antonia Calia-Bogan <alfriadox@gmail.com>
2019-11-22 17:41:38 -0500

    Work on parser, ignore .idea.

--- wright/src/grammar.rs
+++ wright/src/grammar.rs
@@ -1,19 +1,5 @@
-use codespan::{
-    ByteIndex,
-    Span,
-    Files,
-    FileId
-};
-
 pub mod ast;
 pub mod parser;
 
-/// Module for Wright's lexer. Parts of wright's grammar are defined in here, namely:
-///
-/// - `# ...` is a wright single line comment.
-/// - `#* ... *#` is a wright multiline comment.
-/// - `#? ...` is a wright doc comment.
-/// - `## ... ##` is a wright doc comment over multiple lines.
-/// - `#! ...` is a wright doc comment for the module. (like `//!` in rust.)
-///
+/// Module for Wright's lexer.
 pub mod lexer;
