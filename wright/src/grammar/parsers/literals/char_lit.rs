4cb7673b04a4bb3febd746b1be06db07609b0080
Antonia Calia-Bogan <alfriadox@gmail.com>
2020-01-14 12:44:51 -0500

    Testing and bugfixes for char_lit

--- wright/src/grammar/parsers/literals/char_lit.rs
+++ wright/src/grammar/parsers/literals/char_lit.rs
@@ -1,26 +1,24 @@
 use crate::grammar::ast::CharLit;
 use crate::grammar::model::Fragment;
 
-use nom::IResult;
-use nom::sequence::{preceded, terminated};
-use nom::bytes::complete::{tag, take_while_m_n};
-use nom::combinator::{map, recognize, value, map_opt, map_res};
 use nom::branch::alt;
-use nom::character::complete::{char as ch, none_of, anychar};
+use nom::bytes::complete::{tag, take_while_m_n};
+use nom::character::complete::{anychar, char as ch, one_of};
+use nom::combinator::{map, map_opt, map_res, not, recognize, value};
+use nom::sequence::{preceded, terminated};
+use nom::IResult;
 
 impl<'s> CharLit<'s> {
     fn new(frag: Fragment<'s>, inner: char) -> Self {
         CharLit { frag, inner }
     }
 
-    fn unicode_char(frag: Fragment<'s>) -> IResult<Fragment<'s>, char> {
-        preceded(none_of("\\\t\n\r'"), anychar)(frag)
+    pub(super) fn unicode_char(frag: Fragment<'s>) -> IResult<Fragment<'s>, char> {
+        preceded(not(one_of("\\\t\n\r'")), anychar)(frag)
     }
 
-    fn character_body(frag: Fragment<'s>) -> IResult<Fragment, char> {
-        let vch = move |c: char, v: char| {
-            move |fragment: Fragment<'s>| {value(v, ch(c))(fragment)}
-        };
+    pub(super) fn character_body(frag: Fragment<'s>) -> IResult<Fragment, char> {
+        let vch = move |c: char, v: char| move |fragment: Fragment<'s>| value(v, ch(c))(fragment);
         let from_str_radix = |f: Fragment<'s>| u32::from_str_radix(f.source(), 16);
 
         alt((
@@ -40,29 +38,29 @@ impl<'s> CharLit<'s> {
                             preceded(
                                 ch('x'),
                                 map_res(
-                                    take_while_m_n(2,2, |c: char| c.is_ascii_hexdigit()),
-                                    from_str_radix
-                                )
+                                    take_while_m_n(2, 2, |c: char| c.is_ascii_hexdigit()),
+                                    from_str_radix,
+                                ),
                             ),
                             preceded(
                                 tag("u{"),
                                 terminated(
                                     map_res(
-                                        take_while_m_n(1,6, |c: char| c.is_ascii_hexdigit()),
-                                        from_str_radix
+                                        take_while_m_n(1, 6, |c: char| c.is_ascii_hexdigit()),
+                                        from_str_radix,
                                     ),
-                                    ch('}')
-                                )
-                            )
+                                    ch('}'),
+                                ),
+                            ),
                         )),
-                        std::char::from_u32
-                    )
-                ))
-            )
+                        std::char::from_u32,
+                    ),
+                )),
+            ),
         ))(frag)
     }
 
-    fn character_wrapper(frag: Fragment<'s>) -> IResult<Fragment, char> {
+    pub(super) fn character_wrapper(frag: Fragment<'s>) -> IResult<Fragment, char> {
         preceded(tag("'"), terminated(Self::character_body, tag("'")))(frag)
     }
 
