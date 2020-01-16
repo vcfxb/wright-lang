d3595b8c80316c58f73fa67bbb06d53c44b60fc0
Antonia Calia-Bogan <alfriadox@gmail.com>
2020-01-16 18:22:55 -0500

    WIP

--- wright/src/grammar/parsers.rs
+++ wright/src/grammar/parsers.rs
@@ -1,2 +1,6 @@
 /// Wright literal value parsers.
 pub mod literals;
+
+/// Wright expression parsers.
+pub mod expression;
+
--- wright/src/grammar/parsers/literals/boolean_lit_tests.rs
+++ wright/src/grammar/parsers/literals/boolean_lit_tests.rs
@@ -1,1 +1,18 @@
+use codespan::Files;
+use crate::grammar::model::Fragment;
+use crate::grammar::ast::BooleanLit;
 
+#[test]
+fn test_bool_lit() {
+    let mut f: Files<String> = Files::new();
+    let h = f.add("t", "true".to_string());
+    let fr = Fragment::new(&f,h);
+    match BooleanLit::parse(fr) {
+        Ok((rem, bool_lit)) => {
+            assert_eq!(rem.len(), 0);
+            assert_eq!(bool_lit.frag.get_span(), fr.get_span());
+            assert_eq!(bool_lit.inner, true);
+        },
+        Err(e) => {eprintln!("{:#?}", e); panic!();}
+    }
+}
