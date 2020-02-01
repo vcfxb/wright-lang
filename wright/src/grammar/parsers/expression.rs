76916196d950e6495f37e8dbad755898df3e9981
Benjamin Levy <blevy@protonmail.com>
2020-01-31 22:19:22 -0500

    Binary expression parsing (WIP)

--- wright/src/grammar/parsers/expression.rs
+++ wright/src/grammar/parsers/expression.rs
@@ -3,3 +3,12 @@ pub mod identifier;
 
 /// Underscore expression parser.
 pub mod underscore;
+
+/// Expression parser.
+pub mod expression;
+
+/// Binary expression parser.
+pub mod binary_expression;
+
+#[cfg(test)]
+mod binary_expression_tests;
