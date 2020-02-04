d97a6066e7eac74e72f8be7a8f50f7ab8f5c7d02
Antonia Calia-Bogan <alfriadox@gmail.com>
2020-02-04 16:59:08 -0500

    Work on binary expressions

---
+++ wright/src/grammar/parsers/expression/parens.rs
@@ -0,0 +1,14 @@
+use crate::grammar::ast::{Parens, Expression};
+use crate::grammar::model::{HasFragment, Fragment};
+
+impl<'s> Parens<'s> {
+    fn new(frag: Fragment<'s>, inner: Box<Expression<'s>>) -> Self {
+        Self {frag, inner}
+    }
+
+
+}
+
+impl<'s> HasFragment<'s> for Parens<'s> {
+    fn get_fragment(&self) -> Fragment<'s> {self.frag}
+}
