93ac4f2e9a9396bf60826b61c9459e865a87a3c5
Thalia Nero <chrisvn00@gmail.com>
2020-02-15 14:39:22 -0500

    Add expression statement tests

--- wright/src/grammar/parsers/statement/expression_statement_tests.rs
+++ wright/src/grammar/parsers/statement/expression_statement_tests.rs
@@ -2,26 +2,38 @@ use crate::grammar::model::Fragment;
 use crate::grammar::ast::ExpressionSt;
 use codespan::{FileId, Files};
 
-const INPUTS: [&'static str; 2] = [
-    "true;",
-    "false ;",
-];
+fn setup(s: &str) -> (Files<String>, FileId) {
+    let mut f = Files::new();
+    let h = f.add("test", s.to_string());
+    (f, h)
+}
 
-fn setup(inputs: &[&str]) -> (Files<String>, Vec<FileId>) {
-    let mut files: Files<String> = Files::new();
-    let mut vec = Vec::new();
-    for i in 0..inputs.len() {
-        vec.push(files.add(format!("test{}", i), inputs[i].to_string()));
-    };
-    (files, vec)
+fn invalid(s: &str) -> bool {
+    let (files, handle) = setup(s);
+    let fr = Fragment::new(&files, handle);
+    ExpressionSt::parse(fr).is_err()
 }
 
 #[test]
-fn expression_statement() {
-    let (files, handles) = setup(&INPUTS);
-    for handle in handles {
-        let frag = Fragment::new(&files, handle);
-        let (_, _statement) = ExpressionSt::parse(frag).unwrap();
-        // ...
+fn errors() {
+    let inputs = ["true", ";"];
+    for i in &inputs {
+        assert!(invalid(i));
     }
+}
+
+#[test]
+fn nospaces() {
+    let (files, handle) = setup("true;");
+    let fr = Fragment::new(&files, handle);
+    let res = ExpressionSt::parse(fr);
+    assert!(res.is_ok());
+}
+
+#[test]
+fn spaces() {
+    let (files, handle) = setup("false ;");
+    let fr = Fragment::new(&files, handle);
+    let res = ExpressionSt::parse(fr);
+    assert!(res.is_ok());
 }
