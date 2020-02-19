1fb6699c87f28f828dbc8d740f1aef766553295e
Thalia Nero <chrisvn00@gmail.com>
2020-02-18 22:02:47 -0500

    Add comment parsers and tests

---
+++ wright/src/grammar/parsers/whitespace.rs
@@ -0,0 +1,29 @@
+use crate::grammar::model::Fragment;
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete::{char, multispace0, not_line_ending};
+use nom::multi::{count, many0};
+use nom::sequence::{preceded, terminated};
+use nom::IResult;
+
+/// Parses a Wright single line comment
+pub fn line_comment(input: Fragment) -> IResult<Fragment, Fragment> {
+    preceded(
+        count(char('/'), 2),
+        not_line_ending,
+    )(input)
+}
+
+/// Parses a sequence of adjacent whitespace and comments
+/// Returns a vec containing the comment text lines
+pub fn token_delimiter(input: Fragment) -> IResult<Fragment, Vec<Fragment>> {
+    preceded(
+        multispace0,
+        many0(
+            terminated(
+                line_comment,
+                multispace0,
+            ),
+        ),
+    )(input)
+}
