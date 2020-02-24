f11c19c9276083c6d780f0417f3627d5ec9ade0f
Antonia Calia-Bogan <alfriadox@gmail.com>
2020-02-24 16:35:50 -0500

    rustfmt

--- wright/src/grammar/parsers.rs
+++ wright/src/grammar/parsers.rs
@@ -18,37 +18,37 @@ mod whitespace_tests;
 /// This function essentially returns a copy of the parser that returns
 /// `(I, (I, O))`, or `(remaining input, (input consumed, output))`
 pub fn with_input<F, I, O>(parser: F) -> impl Fn(I) -> IResult<I, (I, O)>
-    where
-        I: Clone + Offset + Slice<RangeTo<usize>>,
-        F: Fn(I) -> IResult<I, O> {
+where
+    I: Clone + Offset + Slice<RangeTo<usize>>,
+    F: Fn(I) -> IResult<I, O>,
+{
     move |input: I| -> IResult<I, (I, O)> {
         let i = input.clone();
         match parser(i) {
             Ok((remaining, result)) => {
                 let index = input.offset(&remaining);
                 Ok((remaining, (input.slice(..index), result)))
-            },
-            Err(e) => Err(e)
+            }
+            Err(e) => Err(e),
         }
     }
 }
 
 #[cfg(test)]
 mod with_input_test {
-    use nom::bytes::complete::tag;
     use crate::grammar::parsers::with_input;
+    use nom::bytes::complete::tag;
 
     #[test]
     fn test_with_input() {
         let parser = tag("abc");
         match with_input(parser)("abcdef") {
-            Ok((rem, (consumed,res))) => {
+            Ok((rem, (consumed, res))) => {
                 assert_eq!(rem, "def");
                 assert_eq!(consumed, "abc");
                 assert_eq!(res, "abc");
-            },
-            Err(e) => panic!(e)
+            }
+            Err(e) => panic!(e),
         }
-
     }
 }
