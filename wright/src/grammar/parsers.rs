1ff29c6a70a20662cf9542eecdf1d7b4638fad7a
Benjamin Levy <blevy@protonmail.com>
2020-02-28 16:14:42 -0500

    Add pattern parser and AST

--- wright/src/grammar/parsers.rs
+++ wright/src/grammar/parsers.rs
@@ -4,6 +4,9 @@ pub(crate) mod literals;
 /// Wright expression parsers.
 pub(crate) mod expression;
 
+/// Wright pattern parsers.
+pub(crate) mod pattern;
+
 /// Wright comment and whitespace parsers.
 pub(crate) mod whitespace;
 
