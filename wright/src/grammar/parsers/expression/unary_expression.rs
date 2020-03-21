a2917616e1f7d4e2344618521269cbc935f8d0d1
Antonia Calia-Bogan <alfriadox@gmail.com>
2020-03-21 01:22:40 -0400

    WIP

--- wright/src/grammar/parsers/expression/unary_expression.rs
+++ wright/src/grammar/parsers/expression/unary_expression.rs
@@ -1,5 +1,19 @@
-use crate::grammar::ast::UnaryExpression;
+use crate::grammar::ast::{UnaryExpression, Expression, UnaryOp};
 use crate::grammar::model::{Fragment, HasFragment};
+use crate::grammar::parsers::expression::ToExpression;
+
+impl<'s> UnaryExpression<'s> {
+
+    pub fn parse(input: Fragment<'s>) -> Self {
+
+    }
+}
+
+impl<'s> ToExpression<'s> for UnaryExpression<'s> {
+    fn create_expr(self) -> Expression<'s> {
+        Expression::UnaryExpression(self)
+    }
+}
 
 impl<'s> HasFragment<'s> for UnaryExpression<'s> {
     fn get_fragment(&self) -> Fragment<'s> {
