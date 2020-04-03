17a2c0edcc160a73d261d519cef740965d954a52
Thalia Nero <chrisvn00@gmail.com>
2020-04-03 16:51:34 -0400

    Cannot put generic closures in let

--- wright/src/grammar/model_tests.rs
+++ wright/src/grammar/model_tests.rs
@@ -1,21 +1,18 @@
-use crate::grammar::parsers::testing::setup;
 use crate::grammar::model::Fragment;
-use nom::IResult;
-use std::mem::discriminant;
-use std::ptr::eq as ptr_eq;
-use std::fmt::Debug;
-use nom::Err;
+use crate::grammar::parsers::testing::setup;
+use codespan::Files;
 use nom::bytes::complete::take_while1;
 use nom::character::is_alphabetic;
-use codespan::Files;
+use nom::Err;
+use nom::IResult;
+use std::fmt::Debug;
+use std::mem::discriminant;
+use std::ptr::eq as ptr_eq;
 
-fn test_frag_str<'a, 'b, F1, F2>(
-    s: &'static str,
-    f1: F1,
-    f2: F2)
+fn test_frag_str<'a, F1, F2>(s: &'a str, f1: F1, f2: F2)
 where
-    F1: for<'d> Fn(Fragment<'d>) -> IResult<Fragment<'d>, Fragment<'d>>,
-    F2: Fn(&'b str) -> IResult<&'b str, &'b str>,
+    F1: for<'b> Fn(Fragment<'b>) -> IResult<Fragment<'b>, Fragment<'b>>,
+    F2: Fn(&'a str) -> IResult<&'a str, &'a str>,
 {
     let mut f: Files<String> = Files::new();
     let h = f.add("test", s.to_owned());
@@ -31,10 +28,8 @@ where
             assert!(ptr_eq(rem1.files(), frag.files()));
             assert_eq!(rem1.len(), rem2.len());
             assert_eq!(rem1.get_handle(), frag.get_handle());
-        },
-        (   Result::Err(Err::Error((fr, ek1))),
-            Result::Err(Err::Error((str, ek2)))
-        ) => {
+        }
+        (Result::Err(Err::Error((fr, ek1))), Result::Err(Err::Error((str, ek2)))) => {
             let fr: Fragment = fr;
             assert_eq!(ek1, ek2);
             assert_eq!(fr.get_handle(), frag.get_handle());
@@ -42,16 +37,17 @@ where
             assert_eq!(fr.len(), str.len());
             assert_eq!(fr.source().len(), fr.len());
             assert_eq!(fr.source(), str);
-        },
-        (fr, s) =>
-            panic!("Parser output mismatch!\nFrag parser:\n{:?}\nStr parser:\n{:?}", fr, s)
+        }
+        (fr, s) => panic!(
+            "Parser output mismatch!\nFrag parser:\n{:?}\nStr parser:\n{:?}",
+            fr, s
+        ),
     }
 }
 
 #[test]
 fn test_take_while1() {
     let is_alpha = move |c: char| c.is_alphabetic();
-    let ff = |input: Fragment| take_while1(is_alpha)(input);
-    let fs = take_while1( is_alpha);
-    test_frag_str("", ff, fs);
+    let fs = take_while1(is_alpha);
+    test_frag_str("", |input: Fragment| take_while1(is_alpha)(input), fs);
 }
