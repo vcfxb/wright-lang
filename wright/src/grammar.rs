b0ca47b7841e99e49e4cc1d00f9829362824c46b
Thalia Nero <chrisvn00@gmail.com>
2020-04-03 16:51:34 -0400

    Cannot put generic closures in let

--- wright/src/grammar.rs
+++ wright/src/grammar.rs
@@ -3,7 +3,9 @@ pub mod parsers;
 
 /// Model for Wright's parser system.
 pub mod model;
-#[cfg(test)] mod model_tests;
+
+#[cfg(test)]
+mod model_tests;
 
 /// Model for Wright's Abstract Syntax Tree.
 pub mod ast;
