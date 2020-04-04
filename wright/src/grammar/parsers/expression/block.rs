90a43d8cba70e0a6f63ec10371573ed619cc6d9a
Antonia Calia-Bogan <acaliabogan@gmail.com>
2020-04-04 02:29:59 -0400

    rustfmt

--- wright/src/grammar/parsers/expression/block.rs
+++ wright/src/grammar/parsers/expression/block.rs
@@ -1,39 +1,41 @@
 use crate::grammar::ast::{eq::AstEq, Block, Expression, Statement};
 use crate::grammar::model::{Fragment, HasFragment};
 use crate::grammar::parsers::expression::ToExpression;
-use nom::IResult;
-use nom::combinator::{map, opt};
-use crate::grammar::parsers::with_input;
-use nom::sequence::{pair, terminated, delimited};
-use nom::bytes::complete::tag;
 use crate::grammar::parsers::whitespace::token_delimiter;
+use crate::grammar::parsers::with_input;
+use nom::bytes::complete::tag;
+use nom::combinator::{map, opt};
 use nom::multi::many0;
+use nom::sequence::{delimited, pair, terminated};
+use nom::IResult;
 
 impl<'s> Block<'s> {
     /// Block delimiters. Should not change.
     pub const DELIMITERS: (&'static str, &'static str) = ("{", "}");
 
-    fn inner(input: Fragment<'s>) -> IResult<Fragment<'s>, (Vec<Statement<'s>>, Option<Box<Expression<'s>>>)> {
+    fn inner(
+        input: Fragment<'s>,
+    ) -> IResult<Fragment<'s>, (Vec<Statement<'s>>, Option<Box<Expression<'s>>>)> {
         pair(
             many0(terminated(Statement::parse, token_delimiter)),
-            opt(map(Expression::parse, |e| Box::new(e)))
+            opt(map(Expression::parse, |e| Box::new(e))),
         )(input)
     }
 
     /// Parse a block in source code.
     pub fn parse(input: Fragment<'s>) -> IResult<Fragment<'s>, Self> {
-        map(with_input(delimited(
+        map(
+            with_input(delimited(
                 pair(tag(Self::DELIMITERS.0), token_delimiter),
                 Self::inner,
-                pair(token_delimiter, tag(Self::DELIMITERS.1))
+                pair(token_delimiter, tag(Self::DELIMITERS.1)),
             )),
-            |(consumed, (statements, terminal))| {
-                Self {
-                    frag: consumed,
-                    statements,
-                    result: terminal
-                }
-        })(input)
+            |(consumed, (statements, terminal))| Self {
+                frag: consumed,
+                statements,
+                result: terminal,
+            },
+        )(input)
     }
 }
 
