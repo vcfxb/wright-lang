75f0e645f0bfc47a632373f90c96ab7256e3b922
Antonia Calia-Bogan <acaliabogan@gmail.com>
2020-05-04 15:55:58 -0400

    WIP types

--- wright/src/grammar/ast/types.rs
+++ wright/src/grammar/ast/types.rs
@@ -1,7 +1,37 @@
+use crate::grammar::model::Fragment;
+
 /// A wright primitive type.
-pub enum PrimitiveTypeVariant {}
+/// Wright has almost the same set of primitive types as rust.
+/// The ones here all correspond almost equivalently to their rust
+/// counterparts. The exception is String, which acts more like a
+/// Java String or rust's `&str`.
+#[allow(missing_docs)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub enum PrimitiveTypeVariant {
+    Char,
+    U8, I8, U16, I16, U32, I32, U64, I64, U128, I128,
+    Bool,
+    String,
+}
+
+/// A primitive type in source code.
+#[derive(Copy, Clone, Debug)]
+pub struct PrimitiveType<'s> {
+    /// Associated fragment in source code.
+    pub frag: Fragment<'s>,
+    /// Represented variant.
+    pub variant: PrimitiveTypeVariant
+}
+
+/// A user defined type.
+pub struct UserType<'s> {
+    /// Associated fragment in source code.
+    pub frag: Fragment<'s>
+}
 
 /// A type in source code.
 #[allow(missing_docs)]
 #[derive(Clone, Debug)]
-pub enum Type {}
+pub enum Type<> {
+    Primitive(PrimitiveTypeVariant)
+}
