c22defb9a2600ec3acac69b0eefb13fd65a889a0
Antonia Calia-Bogan <acaliabogan@gmail.com>
2020-05-05 19:03:14 -0400

    bin expr test

--- wright/src/grammar/parsers/expression/binary_expression_tests.rs
+++ wright/src/grammar/parsers/expression/binary_expression_tests.rs
@@ -1,1 +1,13 @@
+use crate::grammar::parsers::testing::test_should_succeed;
+use crate::grammar::ast::BinaryExpression;
 
+fn test_binary_expr_simple() {
+    test_should_succeed(
+        BinaryExpression::parse,
+        "2 + 2",
+        "",
+        |expr| {
+            if let
+        }
+    )
+}
