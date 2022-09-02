3cec6c5d99af86c2dfc3b489d3790684fb38de9c
Venus Xeon-Blonde <alfriadox@gmail.com>
2022-09-01 22:40:42 -0400

    create lexer module

---
+++ wright/src/lexer.rs
@@ -0,0 +1,6 @@
+//! The wright lexer. This module is responsible for lexical analysis and initial processing of source code. 
+
+/// Token of Wright source code. 
+pub enum Token {
+    // TODO
+}
--- wright/src/lib.rs
+++ wright/src/lib.rs
@@ -4,3 +4,4 @@
 //! The wright programming language crate. This is being re-written from the ground up as of September 2022. 
 //! 
 
+pub mod lexer;
