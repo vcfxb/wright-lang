c8cd50f592182bcead2c71c823863c462b9f47ec
Venus Xeon-Blonde <alfriadox@gmail.com>
2022-09-15 01:07:14 -0400

    start lex function

--- wright/src/lexer.rs
+++ wright/src/lexer.rs
@@ -89,4 +89,34 @@ pub enum LexerError {
 /// Read a source file and produce a series of tokens (aka lexemes) representing the source code for transformation into 
 /// an AST. Ignore comments (lines starting with #, anythign between #* and *#). Return error instead of series of tokens
 /// if there is an unfinished sting or character literal. 
-pub fn lex()
+pub fn lex(source: &str) -> Vec<Token> {
+    // Return no tokens if there is no source.
+    if source.is_empty() { return Vec::new(); }
+    // Create output vec.
+    let mut output: Vec<Token> = Vec::new();
+    // For now (until I care enough to go back and optimize this, we'll just iterate through the characters of the string).
+    // We used char_indices here though because I want to know my byte offset into the string.
+    let mut iterator = source.char_indices();
+
+    // Work our way through the iterator using a `while let` loop to destructructure the items as we work through and 
+    // make it slightly clearer that we mutate the iterator during the loop if we find the start of a string.  
+    while let Some((byte_index, character)) = iterator.next() {
+        // Single character tokens are so common that I simplify the function to add them to the output vector here. 
+        let mut emit_single_char_token = |variant: TokenTy| { 
+            output.push(Token { variant, index: byte_index, length: 1}); 
+        };
+
+        // Figure out what type of token to generate here. This may consume an aditional item from the iterator if possible.
+        match character {
+            '(' => emit_single_char_token(TokenTy::LeftParen),
+            ')' => emit_single_char_token(TokenTy::RightParen),
+            '[' => emit_single_char_token(TokenTy::LeftSquare),
+            ']' => emit_single_char_token(TokenTy::RightSquare),
+            '{' => emit_single_char_token(TokenTy::LeftBracket),
+            '}' => emit_single_char_token(TokenTy::RightBracket),
+            _ => unimplemented!()
+        }
+    }
+
+    unimplemented!()
+}
