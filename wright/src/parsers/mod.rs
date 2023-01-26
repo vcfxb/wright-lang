2a1037d69bf1da0226cd7ea9faffe9643f4e6ef2
Venus Xeon-Blonde <alfriadox@gmail.com>
2023-01-25 20:04:56 -0500

    get rid of more empty stuff

--- wright/src/parsers/mod.rs
+++ wright/src/parsers/mod.rs
@@ -1,5 +1,3 @@
 //! Parsers module, for all the parsers implemented by wright and necessary to parse wright source code.
 
-pub mod error;
 pub mod lexer;
-pub mod wright;
--- wright/src/parsers/wright.rs
+++
@@ -1,6 +0,0 @@
-//! Top-level wright syntax parser.
-//!
-
-pub fn parse() {
-    unimplemented!()
-}
