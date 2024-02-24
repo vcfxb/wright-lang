ec2b1f9764efcab5ae7f4daf0674d54e99215e63
Venus Xeon-Blonde <venusflameblonde@gmail.com>
2024-02-24 00:59:28 -0500

    Simplify lexing of trivial tokens

--- wright/src/parser/lexer.rs
+++ wright/src/parser/lexer.rs
@@ -4,165 +4,70 @@
 //! defined for tokens.
 
 use super::fragment::Fragment;
-use derive_more::Display;
 use std::iter::FusedIterator;
 use std::str::Chars;
 use std::{iter::Peekable, ptr};
+use derive_more::Display;
 use unicode_ident::{is_xid_continue, is_xid_start};
 
-/// Constant table of single character tokens and the characters that match them.
-pub const SINGLE_CHAR_TOKENS: &[(char, TokenTy)] = &[
-    ('(', TokenTy::LeftParen),
-    (')', TokenTy::RightParen),
-    ('[', TokenTy::LeftBracket),
-    (']', TokenTy::RightBracket),
-    ('{', TokenTy::LeftCurly),
-    ('}', TokenTy::RightCurly),
-    ('@', TokenTy::At),
-    (';', TokenTy::Semi),
-    ('?', TokenTy::Question),
-    (',', TokenTy::Comma),
-    ('#', TokenTy::Hash),
-    ('$', TokenTy::Dollar),
+/// Trivial tokens that are two ASCII characters and can be matched directly 
+/// against the input source code. 
+pub const TWO_ASCII_TRIVIAL_TOKENS: &[(&[u8; 2], TokenTy)] = &[
+    (b"->", TokenTy::SingleArrow),
+    (b"-=", TokenTy::MinusEq),
+
+    (b"=>", TokenTy::DoubleArrow),
+    (b"==", TokenTy::EqEq),
+
+    (b"&&", TokenTy::AndAnd),
+    (b"||", TokenTy::OrOr),
+    (b"<<", TokenTy::LtLt),
+    (b">>", TokenTy::GtGt),
+    (b"::", TokenTy::ColonColon),
+
+    (b"|=", TokenTy::OrEq),
+    (b"&=", TokenTy::AndEq),
+    (b":=", TokenTy::ColonEq),
+    (b">=", TokenTy::GtEq),
+    (b"<=", TokenTy::LtEq),
+    (b"!=", TokenTy::BangEq),
+    (b"%=", TokenTy::ModEq),
+    (b"^=", TokenTy::XorEq),
+    (b"*=", TokenTy::StarEq),
+    (b"+=", TokenTy::PlusEq),
+    (b"/=", TokenTy::DivEq),
 ];
 
-/// Tokens that can be either a single character or upgraded with an
-/// equals sign.
-pub const POSSIBLE_EQ_UPGRADE_TOKENS: &[(char, TokenTy, TokenTy)] = &[
-    ('!', TokenTy::Bang, TokenTy::BangEq),
-    ('%', TokenTy::Mod, TokenTy::ModEq),
-    ('^', TokenTy::Xor, TokenTy::XorEq),
-    ('*', TokenTy::Star, TokenTy::StarEq),
-    ('+', TokenTy::Plus, TokenTy::PlusEq),
-    ('/', TokenTy::Div, TokenTy::DivEq),
+/// Single ASCII character trivial tokens that can be matched directly against 
+/// the source code. 
+pub const SINGLE_ASCII_CHAR_TRIVIAL_TOKENS: &[(u8, TokenTy)] = &[
+    (b'(', TokenTy::LeftParen),
+    (b')', TokenTy::RightParen),
+    (b'[', TokenTy::LeftBracket),
+    (b']', TokenTy::RightBracket),
+    (b'{', TokenTy::LeftCurly),
+    (b'}', TokenTy::RightCurly),
+    (b'@', TokenTy::At),
+    (b';', TokenTy::Semi),
+    (b'?', TokenTy::Question),
+    (b',', TokenTy::Comma),
+    (b'#', TokenTy::Hash),
+    (b'$', TokenTy::Dollar),
+
+    (b'>', TokenTy::Gt),
+    (b'<', TokenTy::Lt),
+    (b'-', TokenTy::Minus),
+    (b':', TokenTy::Colon),
+    (b'!', TokenTy::Bang),
+    (b'=', TokenTy::Eq),
+    (b'&', TokenTy::And),
+    (b'|', TokenTy::Or),
+    (b'/', TokenTy::Div),
+    (b'+', TokenTy::Plus),
+    (b'^', TokenTy::Xor),
+    (b'*', TokenTy::Star),
+    (b'%', TokenTy::Mod),
 ];
-
-/// Characters that can produce different tokens when followed by an equals sign or themselves.
-pub const POSSIBLE_EQ_OR_DOUBLED_UPGRADE_TOKENS: &[(char, TokenTy, TokenTy, TokenTy)] = &[
-    ('&', TokenTy::And, TokenTy::AndEq, TokenTy::AndAnd),
-    ('|', TokenTy::Or, TokenTy::OrEq, TokenTy::OrOr),
-    ('<', TokenTy::Lt, TokenTy::LtEq, TokenTy::LtLt),
-    ('>', TokenTy::Gt, TokenTy::GtEq, TokenTy::GtGt),
-    (':', TokenTy::Colon, TokenTy::ColonEq, TokenTy::ColonColon),
-];
-
-/// Characters that can produce different tokens when followed by an equals sign or
-/// a `>` for arrows.
-pub const POSSIBLE_EQ_OR_ARROW_UPGRADE_TOKENS: &[(char, TokenTy, TokenTy, TokenTy)] = &[
-    ('-', TokenTy::Minus, TokenTy::MinusEq, TokenTy::SingleArrow),
-    ('=', TokenTy::Eq, TokenTy::EqEq, TokenTy::DoubleArrow),
-];
-
-/// The number of rows of the generated prefix table.
-pub const PREFIX_TABLE_ROWS: usize = {
-    SINGLE_CHAR_TOKENS.len()
-        + 2 * POSSIBLE_EQ_UPGRADE_TOKENS.len()
-        + 3 * POSSIBLE_EQ_OR_DOUBLED_UPGRADE_TOKENS.len()
-        + 3 * POSSIBLE_EQ_OR_ARROW_UPGRADE_TOKENS.len()
-};
-
-/// A relationship between a prefix and the token that should be generated when that prefix matches.
-#[derive(Copy, Clone, Debug)]
-pub struct PrefixToToken {
-    /// An array of two chars. In single char tokens, the second one should be a null character (`'\0'`).
-    /// the char_length field will be used to slice this buffer to get the actual prefix.
-    pub char_buffer: [char; 2],
-    /// The byte length of this prefix and all generated tokens by this prefix.
-    pub byte_len: usize,
-    /// The kind of [Token] generated when this prefix matches.
-    pub kind: TokenTy,
-}
-
-impl PrefixToToken {
-    /// Convenience function to construct a [`PrefixToToken`] by calculating the length of both chars
-    /// (and ignoring the second one if it's null).
-    pub const fn new(chars: [char; 2], kind: TokenTy) -> Self {
-        PrefixToToken {
-            char_buffer: chars,
-
-            byte_len: if chars[1] == '\0' {
-                chars[0].len_utf8()
-            } else {
-                chars[0].len_utf8() + chars[1].len_utf8()
-            },
-
-            kind,
-        }
-    }
-}
-
-/// A full table generated at compile time using all the token tables 
-/// ([SINGLE_CHAR_TOKENS], [POSSIBLE_EQ_UPGRADE_TOKENS], [POSSIBLE_EQ_OR_DOUBLED_UPGRADE_TOKENS], 
-/// [POSSIBLE_EQ_OR_ARROW_UPGRADE_TOKENS]). 
-/// 
-/// This table can be iterated on in order when trying to match a token at the start of a fragment of source code. 
-#[rustfmt::skip]
-pub const PREFIX_TABLE: [PrefixToToken; PREFIX_TABLE_ROWS] = {
-    // Make a mutable table with dummy values to replace with actual values. 
-    let mut table: [PrefixToToken; PREFIX_TABLE_ROWS] = 
-        [PrefixToToken { char_buffer: ['\0'; 2], byte_len: 0, kind: TokenTy::Unknown }; PREFIX_TABLE_ROWS];
-
-    // Current index to insert into table at.
-    let mut write_index: usize = 0;
-
-    // Index used for reading from various tables. 
-    let mut read_index: usize = 0;
-
-    // Iterate first over all the single char tokens. 
-    while read_index < SINGLE_CHAR_TOKENS.len() {
-        // Get row from source table.
-        let (c, token_kind) = SINGLE_CHAR_TOKENS[read_index];
-
-        // Put row in destination table.
-        table[write_index] = PrefixToToken::new([c, '\0'], token_kind);
-
-        // Increment both indices. 
-        read_index += 1;
-        write_index += 1;
-    }
-
-    // Then do all the tokens that can be upgraded with an equals sign. 
-    // Add the row for the token with the equals sign first so that when we iterate over this table in order,
-    // the version without the equals sign does not match prematurely. 
-    read_index = 0;
-    while read_index < POSSIBLE_EQ_UPGRADE_TOKENS.len() {
-        let (c, without_eq, with_eq) = POSSIBLE_EQ_UPGRADE_TOKENS[read_index];
-
-        table[write_index]     = PrefixToToken::new([c, '='], with_eq);
-        table[write_index + 1] = PrefixToToken::new([c, '\0'], without_eq);
-
-        read_index += 1;
-        write_index += 2;
-    }
-
-    // Do the same for the tokens that can be upgraded with an equals sign or doubled. 
-    read_index = 0;
-    while read_index < POSSIBLE_EQ_OR_DOUBLED_UPGRADE_TOKENS.len() {
-        let (c, without_eq, with_eq, doubled) = POSSIBLE_EQ_OR_DOUBLED_UPGRADE_TOKENS[read_index];
-
-        table[write_index]     = PrefixToToken::new([c, c], doubled);
-        table[write_index + 1] = PrefixToToken::new([c, '='], with_eq);
-        table[write_index + 2] = PrefixToToken::new([c, '\0'], without_eq);
-
-        read_index += 1;
-        write_index += 3;
-    }
-
-    // Do the same for possible eq or arrow upgrades.
-    read_index = 0;
-    while read_index < POSSIBLE_EQ_OR_ARROW_UPGRADE_TOKENS.len() {
-        let (c, without_eq, with_eq, with_arrow) = POSSIBLE_EQ_OR_ARROW_UPGRADE_TOKENS[read_index];
-
-        table[write_index]     = PrefixToToken::new([c, '>'], with_arrow);
-        table[write_index + 1] = PrefixToToken::new([c, '='], with_eq);
-        table[write_index + 2] = PrefixToToken::new([c, '\0'], without_eq);
-
-        read_index += 1;
-        write_index += 3;
-    }
-
-    table
-};
 
 /// The pattern that begins any single line comments (including doc comments).
 pub const SINGLE_LINE_COMMENT_PREFIX: &str = "//";
@@ -516,12 +421,16 @@ impl<'src> Lexer<'src> {
         // Ignore any whitespace at the start of the lexer.
         self.ignore_whitespace();
 
+        // If the remaining input is empty, there is no token.
+        if self.remaining.is_empty() {
+            return None;
+        }
+
         // Grab a copy of the initial lexer to compare and check when progress has been made.
         let initial_lexer: Self = self.fork();
 
         // Attempt to parse a single line comment. Return it if it's documentation.
         // Rerun this function if there was a comment and it was ignored successfully.
-        
         match self.handle_single_line_comment() {
             // There was a single line comment ignored or no single line comment.
             None => {
@@ -536,11 +445,6 @@ impl<'src> Lexer<'src> {
 
             // If there was some token, return it.
             token => return token,
-        }
-
-        // If the remaining input is empty, there is no token.
-        if self.remaining.is_empty() {
-            return None;
         }
 
         // Try to handle a multi-line comment if there is one. 
@@ -558,28 +462,31 @@ impl<'src> Lexer<'src> {
             token => return token,
         }
 
-        // To attempt to match a token from the prefix table, make a char iterator
-        // and get two chars from it to test equality. None of the tokens start with a
-        // null character so use that as a single of an unavailable char.
-        {
-            let mut char_iter: Chars = self.remaining.chars();
-            let char_array: [char; 2] = [
-                // Unchecked unwrap here since we know there's at least one char.
-                unsafe { char_iter.next().unwrap_unchecked() },
-                char_iter.next().unwrap_or('\0'),
-            ];
-
-            // Next iterate through the prefix table to try to get any tokens that are covered there.
-            for prefix_meta in PREFIX_TABLE.iter() {
-                // If it's a single char comparison, only compare the first chars.
-                if prefix_meta.char_buffer[1] == '\0' && prefix_meta.char_buffer[0] == char_array[0]
-                {
-                    return Some(self.split_token(prefix_meta.byte_len, prefix_meta.kind));
+        // Do all trivial matching after matching comments to avoid matching "/" for "//".
+        
+        // Attempt to match any two-byte ASCII trivial tokens. 
+        // This must be done before single-ascii byte tokens since matching is greedy. 
+        if self.remaining.len() >= 2 {
+            // Get the first two bytes of the remaining fragment. 
+            // SAFETY: We just checked length. 
+            let bytes: &[u8] = unsafe { self.remaining.inner.as_bytes().get_unchecked(0..2) };
+            // Match against each possible token pattern.
+            for (pattern, kind) in TWO_ASCII_TRIVIAL_TOKENS {
+                if bytes == *pattern {
+                    return Some(self.split_token(2, *kind));
                 }
+            }
+        }
 
-                // Otherwise compare the whole slices.
-                if prefix_meta.char_buffer == char_array {
-                    return Some(self.split_token(prefix_meta.byte_len, prefix_meta.kind));
+        // Do the same for single byte patterns.
+        { 
+            // We can assume there is at least one more byte since we check above if the fragment
+            // is empty and return early if not. 
+            let byte: &u8 = unsafe { self.remaining.inner.as_bytes().get_unchecked(0) };
+
+            for (pattern, kind) in SINGLE_ASCII_CHAR_TRIVIAL_TOKENS {
+                if byte == pattern {
+                    return Some(self.split_token(1, *kind));
                 }
             }
         }
@@ -677,15 +584,7 @@ impl<'src> FusedIterator for Lexer<'src> {}
 #[cfg(test)]
 mod tests {
     use super::Lexer;
-    use super::PREFIX_TABLE;
     use crate::parser::lexer::TokenTy;
-
-    #[test]
-    #[ignore = "this test is just used for debugging the prefix table"]
-    /// Run this with `cargo test manual_debug_prefix_table -- --nocapture --ignored`.
-    fn manual_debug_prefix_table() {
-        dbg!(PREFIX_TABLE);
-    }
 
     #[test]
     fn plus_and_plus_eq_tokens() {
