bc568a0f00dd775a0fc63706baede433f39f2d6f
Venus Xeon-Blonde <venusflameblonde@gmail.com>
2025-01-24 21:38:44 -0500

    Add builder-style methods to help with building parser errors / diagnostics. Add additional note to diagnostics generated by parser errors that are at end-of-source.

--- wright/src/parser/error.rs
+++ wright/src/parser/error.rs
@@ -40,6 +40,15 @@ impl ParserErrorKind {
             ExpectedWhitespace => "expected whitespace character(s)",
         }
     }
+
+    /// Construct a [ParserError] by adding a location [Fragment] to this error variant.
+    pub const fn at(self, f: Fragment) -> ParserError {
+        ParserError {
+            kind: self,
+            location: f,
+            help: None
+        }
+    }
 }
 
 /// An error that occurred while parsing.
@@ -58,13 +67,26 @@ pub struct ParserError {
 }
 
 impl ParserError {
+    /// Builder-style method to add a help string to a [ParserError].
+    pub fn with_help(mut self, help: impl Into<Cow<'static, str>>) -> Self {
+        self.help = Some(help.into());
+        self
+    }
+
     /// Turn this parser error into a full blown compiler error.
     pub fn as_diagnostic(self) -> Diagnostic {
         let description = self.kind.describe();
 
+        // Put a little clarification if the parser reached end of source and then produced an error.
+        let message = if self.location.is_empty_at_end_of_source() {
+            Cow::Borrowed("found end of source here") 
+        } else {
+            Cow::Borrowed("")
+        };
+
         let mut diagnostic = Diagnostic::error()
             .with_message(description)
-            .with_highlights([Highlight::primary(self.location, "")]);
+            .with_highlights([Highlight::primary(self.location.clone(), message)]);
 
         if let Some(help) = self.help {
             diagnostic = diagnostic.with_notes([help]);
