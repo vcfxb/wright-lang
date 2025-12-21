806c61f8b51f81def2613a2a286bebc9b6a008fd
Venus Xeon-Blonde <venusflameblonde@gmail.com>
2025-12-20 23:25:04 -0500

    cleanup: remove dir of old parser modules

--- wright/src/parser/old/ast/statement/bind.rs
+++
@@ -1,8 +0,0 @@
-//! Statements that bind a value to a symbol, possibly with a type, in the context of a scope.
-
-use crate::parser::ast::metadata::AstNodeMeta;
-
-/// Bind statement
-pub struct Bind<'src> {
-    pub meta: AstNodeMeta<'src>,
-}
